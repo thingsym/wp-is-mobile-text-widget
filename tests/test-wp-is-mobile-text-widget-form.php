e13bc4c581e922b4de11ccca4aebbd55078f6e21
thingsym <thingsym@gmail.com>
2017-08-15 23:57:14 +0900

    change indent from spaces to tabs

--- tests/test-wp-is-mobile-text-widget-form.php
+++ tests/test-wp-is-mobile-text-widget-form.php
@@ -2,50 +2,50 @@
 
 class wp_is_mobile_text_widget_Form_Test extends WP_UnitTestCase {
 
-    public function setUp() {
-        parent::setUp();
-        $this->WP_Is_Mobile_Text_Widget = new WP_Is_Mobile_Text_Widget();
-    }
+		public function setUp() {
+				parent::setUp();
+				$this->WP_Is_Mobile_Text_Widget = new WP_Is_Mobile_Text_Widget();
+		}
 
-    /**
-     * @test
-     * @group form
-     */
-    public function form_case_1() {
-        $instance = array(
-            'title' => 'aaaaa',
-            'text' => 'bbbbb',
-            'is_mobile_text' => 'ccccc',
-            'filter' => false,
-        );
+		/**
+		 * @test
+		 * @group form
+		 */
+		public function form_case_1() {
+				$instance = array(
+						'title' => 'aaaaa',
+						'text' => 'bbbbb',
+						'is_mobile_text' => 'ccccc',
+						'filter' => false,
+				);
 
-        ob_start();
-        $this->WP_Is_Mobile_Text_Widget->form( $instance );
-        $form = ob_get_clean();
+				ob_start();
+				$this->WP_Is_Mobile_Text_Widget->form( $instance );
+				$form = ob_get_clean();
 
-        $this->assertContains( 'value="aaaaa"', $form );
-        $this->assertRegExp( '/<textarea.*?>bbbbb<\/textarea>/', $form );
-        $this->assertRegExp( '/<textarea.*?>ccccc<\/textarea>/', $form );
-        $this->assertEquals( 0, preg_match( '/type="checkbox"  checked=\'checked\'/', $form ) );
-    }
+				$this->assertContains( 'value="aaaaa"', $form );
+				$this->assertRegExp( '/<textarea.*?>bbbbb<\/textarea>/', $form );
+				$this->assertRegExp( '/<textarea.*?>ccccc<\/textarea>/', $form );
+				$this->assertEquals( 0, preg_match( '/type="checkbox"	checked=\'checked\'/', $form ) );
+		}
 
-    /**
-     * @test
-     * @group form
-     */
-    public function form_case_2() {
-        $instance = array(
-            'title' => 'aaaaa',
-            'text' => 'bbbbb',
-            'is_mobile_text' => 'ccccc',
-            'filter' => true,
-        );
+		/**
+		 * @test
+		 * @group form
+		 */
+		public function form_case_2() {
+				$instance = array(
+						'title' => 'aaaaa',
+						'text' => 'bbbbb',
+						'is_mobile_text' => 'ccccc',
+						'filter' => true,
+				);
 
-        ob_start();
-        $this->WP_Is_Mobile_Text_Widget->form( $instance );
-        $form = ob_get_clean();
+				ob_start();
+				$this->WP_Is_Mobile_Text_Widget->form( $instance );
+				$form = ob_get_clean();
 
-        $this->assertContains( 'type="checkbox"  checked=\'checked\'', $form );
-    }
+				$this->assertContains( 'type="checkbox"	checked=\'checked\'', $form );
+		}
 
 }
--- tests/test-wp-is-mobile-text-widget-widget_pc.php
+++ tests/test-wp-is-mobile-text-widget-widget_pc.php
@@ -1,38 +1,38 @@
 <?php
 class wp_is_mobile_text_widget_Widget_Pc_Test extends WP_UnitTestCase {
-    protected $backupGlobalsBlacklist = array( 'wpdb', 'wp_query', 'post' );
+		protected $backupGlobalsBlacklist = array( 'wpdb', 'wp_query', 'post' );
 
-    public function setUp() {
-        parent::setUp();
-        $this->WP_Is_Mobile_Text_Widget = new WP_Is_Mobile_Text_Widget();
-    }
+		public function setUp() {
+				parent::setUp();
+				$this->WP_Is_Mobile_Text_Widget = new WP_Is_Mobile_Text_Widget();
+		}
 
-    /**
-     * @test
-     * @group widget
-     * @group pc
-     * @backupGlobals enabled
-     */
-    public function widget_case_pc() {
-        $args = array(
-            'before_widget' => '<aside id="wp_is_mobile_text-1" class="widget widget_is_mobile_text">',
-            'after_widget' => '</aside>',
-            'before_title' => '<h3 class="widget-title">',
-            'after_title' => '</h3>',
-        );
-        $instance = array(
-            'title' => 'aaaaa',
-            'text' => 'bbbbb',
-            'is_mobile_text' => 'ccccc',
-            'filter' => false,
-        );
+		/**
+		 * @test
+		 * @group widget
+		 * @group pc
+		 * @backupGlobals enabled
+		 */
+		public function widget_case_pc() {
+				$args = array(
+						'before_widget' => '<aside id="wp_is_mobile_text-1" class="widget widget_is_mobile_text">',
+						'after_widget' => '</aside>',
+						'before_title' => '<h3 class="widget-title">',
+						'after_title' => '</h3>',
+				);
+				$instance = array(
+						'title' => 'aaaaa',
+						'text' => 'bbbbb',
+						'is_mobile_text' => 'ccccc',
+						'filter' => false,
+				);
 
-        ob_start();
-        $this->WP_Is_Mobile_Text_Widget->widget( $args, $instance );
-        $widget = ob_get_clean();
+				ob_start();
+				$this->WP_Is_Mobile_Text_Widget->widget( $args, $instance );
+				$widget = ob_get_clean();
 
-        $this->assertContains( '<h3 class="widget-title">aaaaa</h3>', $widget );
-        $this->assertContains( '<div class="textwidget">bbbbb</div>', $widget );
-    }
+				$this->assertContains( '<h3 class="widget-title">aaaaa</h3>', $widget );
+				$this->assertContains( '<div class="textwidget">bbbbb</div>', $widget );
+		}
 
 }
