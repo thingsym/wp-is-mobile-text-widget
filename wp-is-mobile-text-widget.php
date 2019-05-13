fff8ffe5ac81c36cb76113d4d403ea331c440d7c
thingsym <thingsym@gmail.com>
2019-05-13 20:25:26 +0900

    docs: fix header

--- wp-is-mobile-text-widget.php
+++ wp-is-mobile-text-widget.php
@@ -1,35 +1,17 @@
 <?php
 /**
  * Plugin Name: WP Is Mobile Text Widget
- * Plugin URI: https://github.com/thingsym/wp-is-mobile-text-widget
+ * Plugin URI:  https://github.com/thingsym/wp-is-mobile-text-widget
  * Description: This WordPress plugin adds text widget that switched display text using wp_is_mobile() function whether the device is mobile or not.
- * Version: 1.0.4
- * Author: thingsym
- * Author URI: https://www.thingslabo.com/
- * License: GPLv2 or later
+ * Version:     1.0.4
+ * Author:      thingsym
+ * Author URI:  https://www.thingslabo.com/
+ * License:     GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: wp-is-mobile-text-widget
  * Domain Path: /languages
  *
  * @package WP_Is_Mobile_Text_Widget
- */
-
-/**
- *     Copyright 2015 thingsym (http://www.thingslabo.com/)
- *
- *     This program is free software; you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation; either version 2 of the License, or
- *     (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with this program; if not, write to the Free Software
- *     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
  */
 
 if ( class_exists( 'WP_Is_Mobile_Text_Widget' ) ) {
